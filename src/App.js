dce0fa8a882b7dfc72dffd38ae4ed2e2e073370c
yusufisbilir <dev.yusufisbilir@gmail.com>
2022-03-11 12:50:06 +0300

    Add react-grid-layout.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import '/node_modules/react-grid-layout/css/styles.css';
+import '/node_modules/react-resizable/css/styles.css';
+
+import GridLayout from 'react-grid-layout';
+
 const App = () => {
-  return <div>App</div>;
+  const layout = [
+    { i: 'a', x: 0, y: 0, w: 1, h: 2, static: false },
+    { i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+    { i: 'c', x: 4, y: 0, w: 1, h: 2 },
+  ];
+  return (
+    <GridLayout
+      className='layout'
+      layout={layout}
+      cols={6}
+      rowHeight={30}
+      width={800}
+    >
+      <div key='a' style={{ backgroundColor: 'red' }}>
+        a
+      </div>
+      <div key='b' style={{ backgroundColor: 'red' }}>
+        b
+      </div>
+      <div key='c' style={{ backgroundColor: 'red' }}>
+        c
+      </div>
+    </GridLayout>
+  );
 };
 
 export default App;
