a90c033c97f37e61ae8a2a6eb0a44021be3937a8
yusufisbilir <dev.yusufisbilir@gmail.com>
2022-03-13 12:19:39 +0300

    Clean up comments.

--- src/App.js
+++ src/App.js
@@ -2,31 +2,37 @@ import '/node_modules/react-grid-layout/css/styles.css';
 import '/node_modules/react-resizable/css/styles.css';
 
 import GridLayout from 'react-grid-layout';
+import { Responsive, WidthProvider } from 'react-grid-layout';
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
 
 const App = () => {
   const layout = [
-    { i: 'a', x: 0, y: 0, w: 1, h: 2, static: false },
-    { i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-    { i: 'c', x: 4, y: 0, w: 1, h: 2 },
+    { i: '1', x: 0, y: 0, w: 1, h: 1 },
+    { i: '2', x: 1, y: 0, w: 1, h: 1 },
+    { i: '3', x: 2, y: 0, w: 1, h: 1 },
   ];
+  const layouts = {
+    lg: layout,
+  };
   return (
-    <GridLayout
+    <ResponsiveGridLayout
+      autoSize={false}
       className='layout'
-      layout={layout}
-      cols={6}
-      rowHeight={30}
-      width={800}
+      layouts={layouts}
+      breakpoints={{ xxl: 1536, xl: 1280, lg: 1024, md: 768, sm: 640 }}
+      cols={{ xxl: 5, xl: 3, lg: 3, md: 2, sm: 1 }}
     >
-      <div key='a' style={{ backgroundColor: 'red' }}>
-        a
+      <div key='1' style={{ background: 'red' }}>
+        1
       </div>
-      <div key='b' style={{ backgroundColor: 'red' }}>
-        b
+      <div key='2' style={{ background: 'green' }}>
+        2
       </div>
-      <div key='c' style={{ backgroundColor: 'red' }}>
-        c
+      <div key='3' style={{ background: 'blue' }}>
+        3
       </div>
-    </GridLayout>
+    </ResponsiveGridLayout>
   );
 };
 
